6fcf4fd2e672b4dd2952e83d77668bf92f8fc9c7
yangx <yangxiuqian95@outlook.com>
2019-08-22 08:27:16 +0800

    my

--- JS/day01/JS14.js
+++
@@ -1,7 +0,0 @@
-function Person(name) { //带参数的构造函数
-    this.name = name;   //定义并初始化name属性
-    this.sayHello = function () {//定义对象方法sayHello
-        alert("hello, I'm " + this.name);
-    };
-};
-
